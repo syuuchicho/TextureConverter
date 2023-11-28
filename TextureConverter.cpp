b83c3cf9313be38e2b5f65e02afd1ee175f3dbc1
syuuchicho <k021g1478@g.neec.ac.jp>
2023-11-28 15:41:43 +0900

    02_06DDSファイルの利用とオプション

--- TextureConverter.cpp
+++ TextureConverter.cpp
@@ -73,6 +73,37 @@ void TextureConverter::SeparateFilePath(const std::wstring& filePath)
 void TextureConverter::SaveDDSTextureToFile()
 {
 	HRESULT result;
+	ScratchImage mipChain;
+	ScratchImage converted;
+
+	//ミップマップ生成
+	result = GenerateMipMaps(
+		scratchImage_.GetImages(),
+		scratchImage_.GetImageCount(),
+		scratchImage_.GetMetadata(),
+		TEX_FILTER_DEFAULT, 0, mipChain);
+	if (SUCCEEDED(result))
+	{
+		//イメージとメタデータを、ミップマップ版で置き換える
+		scratchImage_ = std::move(mipChain);
+		metadata_ = scratchImage_.GetMetadata();
+	}
+
+	//圧縮形式に変換
+	result = Compress(
+		scratchImage_.GetImages(),
+		scratchImage_.GetImageCount(),
+		metadata_,
+		DXGI_FORMAT_BC7_UNORM_SRGB,
+		TEX_COMPRESS_BC7_QUICK |
+		TEX_COMPRESS_SRGB_OUT |
+		TEX_COMPRESS_PARALLEL, 1.0f, converted
+	);
+	if (SUCCEEDED(result))
+	{
+		scratchImage_ = std::move(converted);
+		metadata_ = scratchImage_.GetMetadata();
+	}
 
 	//読み込んだテクスチャをSRGBとして扱う
 	metadata_.format = MakeSRGB(metadata_.format);
